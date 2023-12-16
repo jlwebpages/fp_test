54c31405d84d8265bd1d9bb1756cf722ff4a1ee8
jlwebpages <jerlaguna@gmail.com>
2023-12-16 15:56:29 -0800

    Test.

--- fp_input_form_scripts.js
+++ fp_input_form_scripts.js
@@ -4657,8 +4657,14 @@ function build_post_season_form()
    d.writeln('      if (picks[i] == "")');
    d.writeln('      {');
    d.writeln('         alert("Pick a winner (V or H) for " + visiting_teams[i] + " at " + home_teams[i] + " (Game " + (i+1) + ").");');
-   d.writeln('');
-   d.writeln('         top.focus_element(picks_select_array[i]);');
+
+   d.writeln('alert("top.focus_element(document.fp_inputs.pick"+(i+1)+")");');//jljl
+
+
+   d.writeln('          eval (       "   top.focus_element(document.fp_inputs.pick"+(i+1)+")     "   );');
+
+
+   d.writeln('         //top.focus_element(document.fp_inputs.picks_select_array[i]);');
    d.writeln('');
    d.writeln('         return false;');
    d.writeln('      }');
