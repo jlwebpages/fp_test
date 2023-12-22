119b4d459163c5a3abc785eb288029b1cc56372a
jlwebpages <jerlaguna@gmail.com>
2023-12-22 07:48:45 -0800

    Test.

--- fp_input_form_scripts.js
+++ fp_input_form_scripts.js
@@ -2721,7 +2721,7 @@ function build_regular_season_form()
 
    d.writeln('<tr>');
    d.writeln('<td class="bt2_border" style="text-align: right; padding-right: 5px" colspan="4"><font style="font-size: 12pt">Game '+number_of_rs_games+' Total Points:</font></td>');
-   d.writeln('<td class="bt2_border" style="text-align: left;  padding-left:  5px" colspan="1"><input type=email id="mn_points" class="default_text border_radius" style="width: 35px" name="mn_points" size="2" maxlength="2"></td>');
+   d.writeln('<td class="bt2_border" style="text-align: left;  padding-left:  5px" colspan="1"><input type=email class="default_text border_radius" style="width: 55px" name="mn_points"></td>');
    d.writeln('<td class="bt2_border" colspan="1"></td>');
    d.writeln('</tr>');
    d.writeln('');
@@ -4949,7 +4949,7 @@ function validate_password()
    }
    else
    {
-      alert("Invalid password JL1.");
+      alert("Invalid password JL2.");
       password.value = "";
       top.focus_element(password);
       return false;
