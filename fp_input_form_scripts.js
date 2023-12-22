0d7d236190f491208190e7d5f220e6d6bb247d54
jlwebpages <jerlaguna@gmail.com>
2023-12-21 19:46:38 -0800

    Test.

--- fp_input_form_scripts.js
+++ fp_input_form_scripts.js
@@ -4949,7 +4949,7 @@ function validate_password()
    }
    else
    {
-      alert("Invalid password 1.");
+      alert("Invalid password 2.");
       password.value = "";
       top.focus_element(password);
       return false;
