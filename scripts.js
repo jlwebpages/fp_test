89e7a81a88112dc2b706a486a2b60aa23473131b
jlwebpages <jerlaguna@gmail.com>
2025-06-08 00:56:09 -0700

    Test.

--- scripts.js
+++ scripts.js
@@ -6,21 +6,21 @@ function write_header()
    var d = document;
 
    d.writeln('');
-   d.writeln('');
-   d.writeln('<table style="margin: 0px auto; width: 75%">');
-   d.writeln('');
-   d.writeln('  <tr>');
-   d.writeln('      <td style="text-align: center; font-size: 30pt; letter-spacing: 4; padding-top: 50px; padding-bottom: 50px" colspan=7>DARLENE LAGUNA ART</td>');
+   d.writeln('<table id="header_table_1_desktop" style="margin: 0px auto; width: 75%">');
+   d.writeln('   <tr>');
+   d.writeln('      <td style="text-align: center; font-family: Tahoma; font-size: 30pt; letter-spacing: 4; padding-top: 50px; padding-bottom: 50px">DARLENE LAGUNA ART</td>');
    d.writeln('   </tr>');
-   d.writeln('');
    d.writeln('</table>');
    d.writeln('');
-   d.writeln('<table id="header_table_2" style="margin: 0px auto; width: 75%">');
+   d.writeln('<table id="header_table_1_mobile" style="margin: 0px auto; width: 75%">');
+   d.writeln('   <tr>');
+   d.writeln('      <td style="text-align: center; font-family: Tahoma; font-size: 60pt; letter-spacing: 4; padding-top: 50px; padding-bottom: 50px">DARLENE LAGUNA ART</td>');
+   d.writeln('   </tr>');
+   d.writeln('</table>');
    d.writeln('');
-   //d.writeln('   <span style="padding-bottom: 50px"');
-   d.writeln('');
+   d.writeln('<table id="header_table_2" style="padding-bottom: 50px; margin: 0px auto; width: 75%">');
    d.writeln('   <tr style="vertical-align: top; text-align: center; font-family: lato; color: darkslategray; font-size: 12pt; letter-spacing: 3; max-width: 50%">');
-   d.writeln('      <td style="padding-bottom: 50px">HOME</td>');
+   d.writeln('      <td>HOME</td>');
    d.writeln('      <td>NEW WORK</td>');
    d.writeln('      <td>FEATURED WORK</td>');
    d.writeln('      <td>PHOTO ART</td>');
@@ -28,11 +28,8 @@ function write_header()
    d.writeln('      <td>HOW TO PURCHASE</td>');
    d.writeln('      <td>CONTACT</td>');
    d.writeln('   </tr>');
-   d.writeln('');
-   //d.writeln('   </span>');
-   d.writeln('');
-   d.writeln('');
    d.writeln('</table>');
+   d.writeln('');
 
    d.close();
 
